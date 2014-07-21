96c5d205db27408bef44cc1b2be8af82b1807466
Paul Zaich <pzaich@gmail.com>
2014-07-21 13:50:38 -0700

    load paths correctly

---
+++ lib/doc_ripper.rb
@@ -0,0 +1,10 @@
+require "doc_ripper/version"
+require "doc_ripper/ripper/base"
+require "doc_ripper/text_ripper"
+require "doc_ripper/pdf_ripper"
+require "doc_ripper/docx_ripper"
+require "doc_ripper/ms_doc_ripper"
+
+module DocRipper
+
+end
--- lib/doc_ripper/version.rb
+++ lib/doc_ripper/version.rb
@@ -1,3 +1,5 @@
 module DocRipper
   VERSION = "0.0.1"
 end
+
+
