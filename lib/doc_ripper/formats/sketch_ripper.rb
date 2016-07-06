2ba3456e82046d852e484a1220430ce7b708e90c
Paul Zaich <pzaich@gmail.com>
2016-07-06 00:56:28 -0700

    first pass hackery gets simple sketch file working

--- lib/doc_ripper/formats/sketch_ripper.rb
+++ lib/doc_ripper/formats/sketch_ripper.rb
@@ -1,14 +1,51 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'cfpropertylist'
+
 module DocRipper
   module Formats
     class SketchRipper < Ripper::Base
+
+      class CFPropertyList::CFString
+        def to_s
+          value
+        end
+      end
+
       def read_type
         :mem
       end
 
       def rip
-        puts '@' * 100
         db = SQLite3::Database.new(@file_path)
-        @text ||= db.execute("SELECT value FROM payload").flatten
+        data = db.execute("SELECT value FROM payload").flatten.first
+        @text ||= text_objects(data).join(" ")
+      end
+
+      def blacklist
+        %w(\$null MSAttributedStringFontAttribute NSColor NSParagraphStyle Page)
+      end
+
+      def text_objects(data)
+        objects = CFPropertyList::List.new(data: data).value.value['$objects'].value
+
+        evaluator = Proc.new do |object, previous_object, next_object|
+          digitsRegex = /\{\{\d*, \d*}, \{\d*, \d*\}\}/
+
+          object.is_a?(CFPropertyList::CFString) &&
+            blacklist.select { |bl| object.value.match(/#{bl}/) }.empty? &&
+            !object.value.match(/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/) &&
+            !object.value.match(digitsRegex) &&
+            previous_object.value != "NSFontNameAttribute" &&
+            next_object.is_a?(CFPropertyList::CFDictionary) &&
+            !(previous_object.is_a?(CFPropertyList::CFDictionary) && previous_object.value['$class'].value == 39)
+        end
+
+        objects.select.with_index do |object,i|
+          next_object = objects[i+1]
+          previous_object = objects[i-1]
+          evaluator.call(object, previous_object, next_object)
+        end
       end
     end
   end
