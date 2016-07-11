2a10b190b7e155292e1cf59b0a1d5bfadab89dd1
Paul Zaich <pzaich@gmail.com>
2016-07-10 23:37:02 -0700

    add support for more complex sketch documents

--- lib/doc_ripper/formats/sketch_ripper.rb
+++ lib/doc_ripper/formats/sketch_ripper.rb
@@ -12,6 +12,29 @@ module DocRipper
         end
       end
 
+      class CFPropertyList::CFType
+        def blacklisted_class?
+          return false if !self.value.respond_to?(:[])
+
+          klass = self.value['$class']
+
+          # Sketch Internal ID References
+          # 39 = rectangle / artboard / page / group
+          # 170 = font definition
+
+          return false if !klass
+          [170].include?(klass.value)
+        end
+
+        def sketch_page?
+          return false if !self.value.respond_to?(:[])
+          klass = self.value['$classes']
+
+          return false if !klass
+          klass.is_a?(CFPropertyList::CFArray)
+        end
+      end
+
       def read_type
         :mem
       end
@@ -19,17 +42,17 @@ module DocRipper
       def rip
         db = SQLite3::Database.new(@file_path)
         data = db.execute("SELECT value FROM payload").flatten.first
-        @text ||= text_objects(data).join(" ")
+        @text ||= text_objects(data).join(" ").strip
       end
 
       def blacklist
-        %w(\$null MSAttributedStringFontAttribute NSColor NSParagraphStyle Page)
+        %w(\$null MSAttributedStringFontAttribute NSColor NSParagraphStyle)
       end
 
       def text_objects(data)
         objects = CFPropertyList::List.new(data: data).value.value['$objects'].value
 
-        evaluator = Proc.new do |object, previous_object, next_object|
+        evaluator = Proc.new do |object, previous_object, n_2_previous_object, next_object|
           coordinatesRegex = /\{\{\d*, \d*}, \{\d*, \d*\}\}|\{[\d.e-]*, [\d.]*\}/
 
           object.is_a?(CFPropertyList::CFString) &&
@@ -43,14 +66,17 @@ module DocRipper
             previous_object.value != "NSFontNameAttribute" &&
             # labels always have an dictionary defined afterwards
             next_object.is_a?(CFPropertyList::CFDictionary) &&
-            # Check if the string is defining the name of an artboard
-            !(previous_object.is_a?(CFPropertyList::CFDictionary) && previous_object.value['$class'].value == 39)
-        end
+            # Check if the string is defining the name of an artboard or font
+            !(previous_object.respond_to?(:blacklisted_class?) && previous_object.blacklisted_class?) &&
+            !(n_2_previous_object.respond_to?(:blacklisted_class?) && n_2_previous_object.blacklisted_class?)
+          end
 
         objects.select.with_index do |object,i|
           next_object = objects[i+1]
           previous_object = objects[i-1]
-          evaluator.call(object, previous_object, next_object)
+          n_2_previous_object = objects[i-2]
+
+          evaluator.call(object, previous_object, n_2_previous_object, next_object)
         end
       end
     end
